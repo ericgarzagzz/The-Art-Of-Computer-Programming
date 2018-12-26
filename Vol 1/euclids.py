6893e92014e4501789b4075c2472ca8904735ee1
Eric Garza Gonzalez <eric.garzagnzlz@uanl.edu.mx>
2018-12-26 10:54:32 -0600

    Euclids's Algorithm added

---
+++ Vol 1/euclids.py
@@ -0,0 +1,26 @@
+import random
+
+# Some random positive numbers to test
+m = random.randint(0, 500)
+n = random.randint(0, 500)
+
+# Save original m and n variables before algoritm start to change them
+original_m = m
+original_n = n
+
+r = -1 # r will store the reminder of the division of m and n each time
+
+found = False
+
+while not found:
+	r = m % n # Step 1. Find reminder
+	
+	if r == 0: # Step 2. If reminder is zero then we break the loop
+		found = True
+		break
+	else: # Step 3. Reduce
+		m = n
+		n = r
+		
+		
+print(f"The greatest common divisor of {original_m} and {original_n} is {n}")

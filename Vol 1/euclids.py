cc099c35b206f9df324df8b4756919b7bdb75502
Eric Garza Gonzalez <eric.garzagnzlz@uanl.edu.mx>
2018-12-28 08:49:55 -0600

    Remove unnecessary found variable
    Instead of using a found variable for getting out of the loop, I use the remainder variable.

--- Vol 1/euclids.py
+++ Vol 1/euclids.py
@@ -10,16 +10,13 @@ original_n = n
 
 r = -1 # r will store the reminder of the division of m and n each time
 
-found = False
-
-while not found:
+while r != 0:
 	if m < n: # Extra: Exchange variables when m < n
 		m, n = n, m # Unlike other programming languages, Python is able to exchange variables this way
 	
 	r = m % n # Step 1. Find reminder
 	
 	if r == 0: # Step 2. If reminder is zero then we break the loop
-		found = True
 		break
 	else: # Step 3. Reduce
 		m = n
